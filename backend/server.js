657ca29053604ccc353f66f9ef3cc06c1e4b01b7
AravindSakthivel <aravind.sakthi@outlook.com>
2021-05-17 11:42:51 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,12 @@ const transactRouter = require("./routes/transactRouter");
 const app = express();
 
 dotenv.config();
-// // Serve static files from the React frontend app
-// app.use(express.static(path.join(__dirname, "../frontend/build")));
-// // Anything that doesn't match the above, send back index.html
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "../frontend/build/index.html"));
-// });
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+// Anything that doesn't match the above, send back index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "../frontend/build/index.html"));
+});
 mongoose.connect(
   process.env.MONGO_URI,
   {
