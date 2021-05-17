e3b0ecbbf63c322fb4cfead38210f54fcdb6c1b9
AravindSakthivel <aravind.sakthi@outlook.com>
2021-05-17 13:02:20 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -56,14 +56,12 @@ app.use("/api/auth", authRouter);
 
 app.use("/api/transact", transactRouter);
 
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname + "../frontend/build/index.html"));
-  });
-}
+// Serve any static files
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+// Handle React routing, return all requests to React app
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname + "../frontend/build/index.html"));
+});
 
 const port = process.env.PORT || 5000;
 
